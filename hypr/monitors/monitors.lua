8ef6bbd89db5118954579d1840d327a982af3c1d
Devitana <opreav.stelian+jetbrains@gmail.com>
2026-08-09 18:48:09 +0200

    Migrate to lua , deleting conf files, monitor for all

--- hypr/monitors/monitors.lua
+++ hypr/monitors/monitors.lua
@@ -1,25 +1,8 @@
 -- Monitors setup for my triple monitor setup. This is a 3x 2560x1440@144hz setup with the left and right monitors rotated to portrait mode.
 
--- left monitor
 hl.monitor({
-    output = "DP-3",
-    mode = "2560x1440@144",
-    position = "0x0",
-    scale = 1
-})
-
--- center monitor
-hl.monitor({
-    output = "DP-2",
-    mode = "2560x1440@144",
-    position = "2560x0",
-    scale = 1
-})
-
--- right monitor
-hl.monitor({
-    output = "DP-1",
-    mode = "2560x1440@144",
-    position = "5120x0",
+    output = ",
+    mode = "",
+    position = "",
     scale = 1
 })
