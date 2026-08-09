3547437ab206c3d5422c703b1c5b0d67cb2bf42c
Devitana <opreav.stelian+jetbrains@gmail.com>
2026-08-09 18:16:56 +0200

    Delete hypr/lua directory

--- hypr/lua/programs.lua
+++
@@ -1,8 +0,0 @@
--- config.lua  –  programs ($variable definitions)
--- Generates: hypr/autostart/programs.conf
-
-hl.heading("MY PROGRAMS")
-hl.comment("Set programs that you use")
-hl.variable("terminal",    "kitty")
-hl.variable("fileManager", "nautilus")
-hl.variable("menu",        "hyprlauncher")
