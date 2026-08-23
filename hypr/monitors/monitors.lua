93ea82c276c2cce1e222ebe6fea85eceb2fdde56
Devitana <opreav.stelian+jetbrains@gmail.com>
2026-08-23 10:23:47 +0200

    Correct output parameter in monitors.lua
    Fix output parameter in monitor setup.

--- hypr/monitors/monitors.lua
+++ hypr/monitors/monitors.lua
@@ -1,7 +1,7 @@
 -- Monitors setup for my triple monitor setup. This is a 3x 2560x1440@144hz setup with the left and right monitors rotated to portrait mode.
 
 hl.monitor({
-    output = ",
+    output = "",
     mode = "",
     position = "",
     scale = 1
